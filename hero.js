938ebe063ebb02eed06eaf2e5222785b77ef1cac
Paloma Silva <paloma11ts@gmail.com>
2025-08-25 22:18:59 -0300

    inicializando projeto

---
+++ hero.js
@@ -0,0 +1,9 @@
+class Hero {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+}
+
+export default Hero;
---
+++ index.js
@@ -0,0 +1,4 @@
+import Hero from './hero.js';
+
+const ana = new Hero('Ana', 25, 'mago');
+console.log(ana);
